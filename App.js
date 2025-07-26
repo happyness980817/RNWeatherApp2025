ca7f9fc3f184b9b9a1d1afd092918105080ce497
Paul Jang <happyness980817@gmail.com>
2025-07-26 14:27:18 +0900

    layout system

--- App.js
+++ App.js
@@ -1,28 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-      <StatusBar style="light" />
-    </View>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    </View> // 모든 View 는 flex container
+    // RN 에서 flex direction 의 기본값은 column
   );
-} // Rule #1 - div 대신 View 사용. div 는 쓸 수 없음
-// Rule #2 - 모든 텍스트는 Text component 내부에 위치
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-    color: "white",
-  },
-});
-
-// Rule #3 - StyleSheet.create 는 단순히 style object 를 생성해주는 역할이지만
-// 자동완성 지원, 컴포넌트 분리 때문에 사용
+}
